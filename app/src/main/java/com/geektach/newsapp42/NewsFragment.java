777eba2126bd4b6d64f4e308c2435f6b2febf5e7
Erbole <95484654+Erbole@users.noreply.github.com>
2022-04-14 03:36:55 +0300

    Initial commit

---
+++ app/src/main/java/com/geektach/newsapp42/NewsFragment.java
@@ -0,0 +1,55 @@
+package com.geektach.newsapp42;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.geektach.newsapp42.databinding.FragmentHomeBinding;
+import com.geektach.newsapp42.databinding.FragmentNewsBinding;
+import com.geektach.newsapp42.models.Article;
+
+public class NewsFragment extends Fragment {
+
+    private FragmentNewsBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentNewsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
+    }
+
+    private void save() {
+        String text = binding.editText.getText().toString();
+        Bundle bundle = new Bundle();
+        Article article = new Article(text, System.currentTimeMillis());
+        bundle.putSerializable("article",article);
+        getParentFragmentManager().setFragmentResult("rk_news", bundle);
+        close();
+    }
+    private void close() {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_activity_main);
+        navController.navigateUp();
+    }
+}
